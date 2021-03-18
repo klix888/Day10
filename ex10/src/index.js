eb676dbac8dfc5ec3fd397523c09cfc7090d8eca
Dzevad <ynwa886@gmail.com>
2021-03-18 01:03:33 +0100

    Complex Objects

---
+++ ex10/src/index.js
@@ -0,0 +1,29 @@
+function myFunction() {
+    var myMusic = {
+        001: {
+            artist: "Billy Joel",
+            title: "Piano Man",
+            release_year: 1973,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            gold: true
+        },
+        002: {
+            artist: "Dino Merlin",
+            title: "Sredinom",
+            release_year: 2000,
+            formats: {
+                1: "CF",
+                2: "8Y",
+                3: "LR"
+            },
+            gold: false
+        }
+    }
+    return myMusic;
+}
+console.log(myFunction()[2]);
+module.exports = myFunction;
